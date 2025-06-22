c8dbda90526b0b40c95faba5e08364d3d82b1b10
Anastasiia Santalova <anastasiia_santalova@epam.com>
2025-06-22 20:05:08 -0300

    0013: button component updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
-import { CommonButtonComponent } from './components/common-button/common-button.component';
+import { CommonButtonComponent } from './components/button/button.component';
 import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
-import { CloseButtonComponent } from './components/close-button/close-button.component';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
@@ -29,7 +28,6 @@ const routes: Routes = [
 		HeaderComponent,
 		CommonButtonComponent,
 		ModalDialogComponent,
-		CloseButtonComponent,
   SkillsPageComponent,
   ContactsPageComponent,
   FooterComponent
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonTypes } from 'src/app/types/types';
+
+@Component({
+	selector: 'app-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss']
+})
+export class CommonButtonComponent {
+	@Input() name?: string = '';
+  @Input() type?: ButtonTypes = 'cta';
+  @Input() icon?: string = '';
+  @Input() disabled?: boolean = false;
+  @Output() onButtonClick = new EventEmitter();
+
+	onClick(event: Event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+		this.onButtonClick.emit();
+	}
+
+  constructor(private elem: ElementRef) {}
+
+  ngAfterViewInit() {
+    if (this.icon) {
+      const target = this.elem.nativeElement.querySelector('.icon');
+
+      if (target) {
+        target.style.maskImage = `url('/assets/icons/${this.icon}.svg')`;
+      }
+    }
+  }
+}
