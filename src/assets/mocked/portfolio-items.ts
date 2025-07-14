8682520766db9df71c2c2d52f2636e4ee5044d36
Anastasiia Santalova <anastasiia_santalova@epam.com>
2025-07-14 17:01:38 -0300

    0017: FE changes, new images, portfolio updates

--- src/assets/mocked/portfolio-items.ts
+++ src/assets/mocked/portfolio-items.ts
@@ -1,27 +1,17 @@
 export const PORTFOLIO_ITEMS = [
     {
-        name: "Project Name 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        src: "https://plus.unsplash.com/premium_photo-1673795753320-a9df2df4461e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        name: "Sensiya",
+        description: "Sensiya is my mindful baby: a tiny web space for people who want to slow down, breathe, and reconnect with themselves. It’s all about calm vibes, gentle design, and small features that make you smile — kind of like a warm cup of tea for your soul.",
+        src: "https://anas-b1197.web.app/images/sensiya.png"
     },
     {
-        name: "Project Name 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        src: "https://plus.unsplash.com/premium_photo-1673795753320-a9df2df4461e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        name: "SpiritUX",
+        description: "SpiritUX is my homemade Angular component library — because why use boring buttons when you can craft your own? It’s a playful toolkit for building beautiful, reusable UI pieces. Devs deserve cute things too!",
+        src: "https://anas-b1197.web.app/images/library.png"
     },
     {
-        name: "Project Name 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        src: "https://plus.unsplash.com/premium_photo-1673795753320-a9df2df4461e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-        name: "Project Name 4",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        src: "https://plus.unsplash.com/premium_photo-1673795753320-a9df2df4461e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-        name: "Project Name 5",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        src: "https://plus.unsplash.com/premium_photo-1673795753320-a9df2df4461e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        name: "SimplyHealthyStore",
+        description: "SimplyHealthyStore is my old student project where I flexed my Angular muscles. It’s got a product catalog, add-to-cart, remove-from-cart — all the basics. The checkout form? Still MIA. One day, I might revive it — if coffee and motivation align!",
+        src: "https://anas-b1197.web.app/images/simply-healthy-store.png"
     }
 ]
